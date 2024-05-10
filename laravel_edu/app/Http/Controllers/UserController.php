029975e19b75d3a15bf9fe3e32cac6253086427a
bobaeiiie <poidottee@gmail.com>
2024-05-10 12:52:41 +0900

    240510 laravel com

--- laravel_edu/app/Http/Controllers/UserController.php
+++ laravel_edu/app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -9,7 +10,12 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function eduUser() {
+        
+        // ----------------------------
         // Query Builder
+        // ----------------------------
+
+
         // $result = DB::select('select * from users');
 
         // select
@@ -55,6 +61,7 @@ class UserController extends Controller
         // ----------------------------
         // 쿼리 빌더 체이닝
         // ----------------------------
+
         // users 테이블 모든 데이터 조회
         // $result = DB::table('users')->get();
         // $result = DB::table('users')->where('name', '=','홍길동')->get()->dd();
@@ -73,15 +80,131 @@ class UserController extends Controller
         //         ->get();
 
         // select name from users order by name ASE;
-        $result = DB::table('users')
-                ->select('name')
-                ->orderBy('name', 'ASC')
-                ->get();
+        // $result = DB::table('users')
+        //         ->select('name')
+        //         ->orderBy('name', 'ASC')
+        //         ->get();
 
-        // WHERE 나머지는 내일
+        // select * from users where id in (?, ?); [2, 5]
+        // $result = DB::table('users')
+        //         ->whereIn('id', [2, 5])
+        //         ->get();
+
+        // select * from users where deleted_at is null
+        // $result = DB::table('users')
+        //         ->whereNull('deleted_at')
+        //         ->get();
+
+        // 2023년에 가입한 사람만 출력
+        // select * from users where year(created_at) = ? [2023]
+        // select * from users created_at between 20230101000000 and 20231231235959
+        // $result = DB::table('users')
+        //         ->whereYear('created_at', '2023')
+        //         ->get()->dd();
+
+
+        // 성별당 회원의 수를 구하자
+        // SELECT gender, COUNT(gender) FROM users GROUP BY gender;
+        // $result = DB::table('users')
+        //         ->select('gender', DB::raw('count(gender)'))
+        //         ->groupBy('gender')
+        //         ->having('gender', '=', 'M')
+        //         ->get();
+
+
+        // select id, name from users order by id limit ? offset ?; [1, 2]
+        // $result = DB::table('users')
+        //         ->select('id', 'name')
+        //         ->orderBy('id')
+        //         ->limit(1)
+        //         ->offset(2)
+        //         ->get();
+
+        // 실행 메소드 first() : 쿼리 결과에서 가장 첫번째 레코드만 반환
+        $result = DB::table('users')->first();
+
+        // 실행 메소드 count() : 쿼리 결과의 레코드 수를 반환
+        $result = DB::table('users')->count();
+
+        // 실행 메소드 find() : 지정된 기본 키에 해당하는 레코드를 반환
+        $result = DB::table('users')->find(4);
+
+
+
+
+        // $reqData = null; // 유저가 1 또는 2인 데이터 전달
+        // $result = DB::table('users')
+        //         ->when($reqData, function($query, $reqData) {
+        //             $query->where('id', $reqData);
+        //         })
+        //         ->dd();
+        
+        // insert
+        // $result = DB::table('users')->insert([
+        //     'name' => '김영희'
+        //     ,'email' => 'kim@admin.com'
+        //     ,'password' => Hash::make('qwer1234!')
+        //     ,'gender' => 'F'
+            
+        // ]);
+
+        // update
+        // $result = DB::table('users')
+        //         ->where('id', '15')
+        //         ->update([
+        //             'email' => 'kim@naver.com'
+        // ]);
+
+        // delete
+        // $result = DB::table('users')
+        //         ->where('id', '15')
+        //         ->delete();
+
+
+        // ----------------------
+        // Eloquent Model
+        // ----------------------
+
+        // select
+        // $result = User::all(); 
+        // $result = User::find(4); 
+
+        // upsert > insert (create)
+        // $data = [
+        //     'name' => '김영희'
+        //     ,'email' => 'kim@naver.com'
+        //     ,'password' => Hash::make('qwer1234!')
+        //     ,'gender' => 'F'
+        // ];
+        // $result = User::create($data);
+
+        // upsert > update / (save)
+        // DB::beginTransaction();
+        // $target = User::find(17);
+        // $target->gender = 'M';
+        // $result = $target->save();
+        // DB::beginTransaction();
+
+        // delete
+        // $result = User::destroy(17);
+
+        // soft delete된 데이터 조회하는 방법
+        $result = User::withTrashed()->get(); // 소프트 딜리트 포함
+        $result = User::onlyTrashed()->get(); // 소프트 딜리트만
+
+        // soft delete된 데이터 복원하는 방법
+        $result = User::where('id', 17)->restore();
+        
+
+
+
+
+
+
 
 
 
         return var_dump($result);
+
     }
 }
--- laravel_edu/app/Models/User.php
+++ laravel_edu/app/Models/User.php
@@ -4,24 +4,41 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    // 트레이트
+    // 해당 모델에 소프트딜리트를 적용시키고 싶으면 SoftDeletes 추가
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
+    // // 모델과 이어질 테이블 명을 정의하는 프로퍼티
+    // protected $table = 'user';
+
+    // // pk를 지정하는 프로퍼티
+    // protected $primaryKey = 'id';
+
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
+    // fillable 화이트리스트 : upsert 시 변경 허용할 컬럼을 설정하는 프로퍼티
     protected $fillable = [
         'name',
         'email',
         'password',
+        'gender',
     ];
+
+    // guarded 블랙리스트 : upsert 시 변경 비허용할 컬럼을 설정하는 프로퍼티
+    // protected $guarded = [
+    //     'id'
+    // ];
+
 
     /**
      * The attributes that should be hidden for serialization.
